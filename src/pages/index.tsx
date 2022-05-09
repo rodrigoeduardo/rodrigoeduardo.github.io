475962d9c5eb1ca2fe8dde76442e6c516d808715
Rodrigo Eduardo <rodrigoow99@hotmail.com>
2022-05-09 19:56:18 -0300

    ContactSection added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,21 @@
-import type { NextPage } from "next";
-import Head from "next/head";
-import { AboutMeSection } from "../components/AboutMeSection";
-import { HomeSection } from "../components/HomeSection";
-import { PortfolioSection } from "../components/PortfolioSection";
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import { AboutMeSection } from '../components/AboutMeSection';
+import { ContactSection } from '../components/ContactSection';
+import { HomeSection } from '../components/HomeSection';
+import { PortfolioSection } from '../components/PortfolioSection';
 
 const Home: NextPage = () => {
   return (
     <>
       <Head>
         <title>Portfolio | Rodrigo Eduardo</title>
+        <link rel="shortcut icon" href="assets/favicon.png" />
       </Head>
       <HomeSection />
       <AboutMeSection />
       <PortfolioSection />
+      <ContactSection />
     </>
   );
 };
