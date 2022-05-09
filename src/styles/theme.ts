9cee3398d76b84adddde9d9ee16ee45d10eda7d3
Rodrigo Eduardo <rodrigoow99@hotmail.com>
2022-05-09 19:53:23 -0300

    breakpoints added

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,19 +1,25 @@
 import { extendTheme } from '@chakra-ui/react';
 
+const breakpoints = {
+  base: '0px',
+  xsm: '360px',
+  sm: '490px',
+  md: '768px',
+  lg: '1024px',
+  xl: '1400px',
+};
+
 export const theme = extendTheme({
-  colors: {
-    black: {
-      300: '#01161E',
-    },
-    purple: {
-      500: '#7067CF',
-      50: '#B7C0EE',
-    },
-  },
+  breakpoints,
   styles: {
     global: {
       html: {
         scrollBehavior: 'smooth',
+        fontSize: {
+          base: '65%',
+          md: '80%',
+          lg: '100%',
+        },
       },
       body: {
         bgColor: 'blue.900',
